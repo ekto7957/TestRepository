c24626d78f565f24001a95b740ba6ebd1981ebfb
HYUN JUN PARK <ekto7957@gmail.com>
2025-02-04 11:28:23 +0900

    3

--- L250204_1/Program.cs
+++ L250204_1/Program.cs
@@ -2,6 +2,18 @@
 {
     internal class Program
     {
+        static uint A = 1103515245;
+        static uint C = 12345;
+        static uint M = 2147483648;
+
+        static uint seed = 1;
+
+        static uint rand()
+        {
+            seed = (A * seed + C) % M;
+            return seed;
+        }
+
         static void Main(string[] args)
         {
             //  1-52 정수를 배열에 넣고 
@@ -55,6 +67,84 @@
 
             //// 앞에서 8개 출력
             //Console.WriteLine("랜덤으로 선택된 숫자: " + string.Join(", ", arrNum[0..8]));
+
+            // 난수 생성기 (시간을 시드로 사용; 보안성이 크게 필요하지 않은 경우)
+            Random rand2 = new Random();
+
+            // 1~52의 값을 가진 트럼프 덱 생성
+            int[] deck = new int[52];
+            for (int i = 0; i < deck.Length; i++)
+            {
+                deck[i] = i + 1;
+            }
+
+            // 덱 섞기 (Shuffle)
+            for (int i = 0; i < deck.Length * 10; ++i)
+            {
+                int firstCardIndex = rand2.Next(0, deck.Length);
+                int secondCardIndex = rand2.Next(0, deck.Length);
+
+                int temp = deck[firstCardIndex];
+                deck[firstCardIndex] = deck[secondCardIndex];
+                deck[secondCardIndex] = temp;
+            }
+
+            // 카드 번호를 문양과 숫자로 변환하는 함수 호출 후 8장 출력
+            for (int i = 0; i < 8; ++i)
+            {
+                string card = GetCardName(deck[i]);
+                Console.WriteLine(card);
+            }
+
+        }
+
+        // 카드 번호를 받아서 문양과 숫자(면)를 문자열로 반환하는 함수
+        static string GetCardName(int cardNumber)
+        {
+            string suit = "";
+            int rank = ((cardNumber - 1) % 13) + 1; // 순위: 1~13
+
+            // 문양 결정
+            if (cardNumber >= 1 && cardNumber <= 13)
+            {
+                suit = "♥";
+            }
+            else if (cardNumber >= 14 && cardNumber <= 26)
+            {
+                suit = "◆";
+            }
+            else if (cardNumber >= 27 && cardNumber <= 39)
+            {
+                suit = "♣";
+            }
+            else if (cardNumber >= 40 && cardNumber <= 52)
+            {
+                suit = "♠";
+            }
+
+            // 숫자(면) 결정: 1=A, 11=J, 12=Q, 13=K, 그 외는 숫자 그대로.
+            string rankStr = "";
+            switch (rank)
+            {
+                case 1:
+                    rankStr = "A";
+                    break;
+                case 11:
+                    rankStr = "J";
+                    break;
+                case 12:
+                    rankStr = "Q";
+                    break;
+                case 13:
+                    rankStr = "K";
+                    break;
+                default:
+                    rankStr = rank.ToString();
+                    break;
+            }
+
+            // 예: "Heart A", "Diamond 10" 등
+            return suit + " " + rankStr;
         }
     }
 }
